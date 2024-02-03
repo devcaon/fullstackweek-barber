ee497e44d28d74d355bec3e9acdcca1f2d29c573
Claudio Oliveira <dev.caon@gmail.com>
2024-02-02 22:29:50 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,18 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/Header";
+import { format } from 'date-fns';
 
 export default function Home() {
   return (
-   <div>
-    <Header />
-   </div>
+    <div>
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Gabriela!</h2>
+        <p className="text-sm">
+          <span className="capitalize">{format(new Date(), "EEEE", { locale: ptBR })}</span>
+          {format(new Date(), "',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
+    </div>
   );
 }
